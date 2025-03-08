f6019bf11335b38c1c8e111a3c7df2f351c79176
Rafael222-R <128435934+Rafael222-R@users.noreply.github.com>
2025-03-07 23:20:57 -0300

    atividade

--- exercicios/exercicios-javaScript/4exercicio.js
+++ exercicios/exercicios-javaScript/4exercicio.js
@@ -2,4 +2,3 @@ for (let number = 0; number < number.length; number++) {
     const element = array[number];
     console.log(element)
 }
-
---
+++ exercicios/exercicios-javaScript/5exercicio.js
@@ -0,0 +1,14 @@
+let numA = 5
+
+
+const dobro = numA
+    console.log( (numA * numA));
+
+
+const triplo = numA
+    console.log( (numA * numA * numA))
+
+const quadruplo = numA
+    console.log(numA * numA * numA * numA)
+
+
