ef8362aef22e4c6d9bd914c4f27a710a224b7cdb
Rafael222-R <rafael_dias18@hotmail.com>
2025-03-11 22:01:36 -0300

    Atividade em sala

---
+++ exercicios/exercicioNPM/calculadora-projeto/calculadora.js
@@ -0,0 +1,17 @@
+
+
+export const soma = (numA, numB) => {
+ return numA + numB
+}
+
+export const subtracao = (numA, numB) => {
+    return numA - numB
+}
+
+export const multiplicacao = (numA, numB) => {
+    return numA * numB
+}
+
+export const dividir = (numA, numB) => {
+    return numA / numB
+}
---
+++ exercicios/exercicioNPM/calculadora-projeto/index.js
@@ -0,0 +1,7 @@
+import {soma, subtracao, dividir, multiplicacao } from "./calculadora.js";
+
+
+console.log("Soma: ", soma(10, 5));
+console.log("Subtração: ", subtracao(10, 5));
+console.log("Multiplicação: ", multiplicacao(10, 5));
+console.log("Divisão: ", dividir(10, 5));
