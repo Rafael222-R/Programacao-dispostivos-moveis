15aca7b58a4fbca2e54088d3903c335ac50bdba4
Rafael222-R <rafael_dias18@hotmail.com>
2025-05-09 21:49:55 -0300

    atividade aula 10

---
+++ exercicios/futebol-drawer/src/screens/EscudoScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function EscudoScreen() {
+  return (
+    <View>
+      <Text>EscudoScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ exercicios/futebol-drawer/src/screens/JogadoresScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function JogadoresScreen() {
+  return (
+    <View>
+      <Text>JogadoresScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
